9d4b453a94a43e6bd54e9547ff2e97ec096ad712
Artur Golovko <arturgolovko37@gmail.com>
2023-07-30 22:44:05 +0300

    KefF

--- src/components/NavBBar2/NavigationVcard2.vue
+++ src/components/NavBBar2/NavigationVcard2.vue
@@ -21,6 +21,94 @@
         </div>
       </v-expand-transition>
     </v-card>
+    <v-card color="rgb(37, 56, 74)" elevation="0" style="margin-top: 8px;"> 
+          <v-card-actions height="40" @click="show = !show" class=" ma-2">
+            <v-list-item rounded="lg" style="color: #ffffff;"   title="Tests" ></v-list-item>
+
+      <v-spacer></v-spacer>
+
+      <v-btn 
+      height="40"
+      width="40"
+      color="white"
+        :icon="show ? 'mdi-chevron-down' : 'mdi-chevron-right'"
+        
+      ></v-btn>
+    </v-card-actions>
+    <v-expand-transition>
+      <div v-show="show">
+        <div style="max-width: 30px;">
+                    ihihihihihih
+                </div>
+        </div>
+      </v-expand-transition>
+    </v-card>
+    <v-card color="rgb(37, 56, 74)" elevation="0" style="margin-top: 8px;"> 
+          <v-card-actions height="40" @click="show = !show" class=" ma-2">
+            <v-list-item rounded="lg" style="color: #ffffff;"   title="Tests" ></v-list-item>
+
+      <v-spacer></v-spacer>
+
+      <v-btn 
+      height="40"
+      width="40"
+      color="white"
+        :icon="show ? 'mdi-chevron-down' : 'mdi-chevron-right'"
+        
+      ></v-btn>
+    </v-card-actions>
+    <v-expand-transition>
+      <div v-show="show">
+        <div style="max-width: 30px;">
+                    ihihihihihih
+                </div>
+        </div>
+      </v-expand-transition>
+    </v-card>
+    <v-card color="rgb(37, 56, 74)" elevation="0" style="margin-top: 8px;"> 
+          <v-card-actions height="40" @click="show = !show" class=" ma-2">
+            <v-list-item rounded="lg" style="color: #ffffff;"   title="Tests" ></v-list-item>
+
+      <v-spacer></v-spacer>
+
+      <v-btn 
+      height="40"
+      width="40"
+      color="white"
+        :icon="show ? 'mdi-chevron-down' : 'mdi-chevron-right'"
+        
+      ></v-btn>
+    </v-card-actions>
+    <v-expand-transition>
+      <div v-show="show">
+        <div style="max-width: 30px;">
+                    ihihihihihih
+                </div>
+        </div>
+      </v-expand-transition>
+    </v-card>
+    <v-card color="rgb(37, 56, 74)" elevation="0" style="margin-top: 8px;"> 
+          <v-card-actions height="40" @click="show = !show" class=" ma-2">
+            <v-list-item rounded="lg" style="color: #ffffff;"   title="Tests" ></v-list-item>
+
+      <v-spacer></v-spacer>
+
+      <v-btn 
+      height="40"
+      width="40"
+      color="white"
+        :icon="show ? 'mdi-chevron-down' : 'mdi-chevron-right'"
+        
+      ></v-btn>
+    </v-card-actions>
+    <v-expand-transition>
+      <div v-show="show">
+        <div style="max-width: 30px;">
+                    ihihihihihih
+                </div>
+        </div>
+      </v-expand-transition>
+    </v-card>
 
 </template>
 
