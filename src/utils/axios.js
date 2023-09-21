6a2247d2775d90e3624506947292bc2786d25550
Artur Golovko <arturgolovko37@gmail.com>
2023-09-21 20:28:49 +0300

    KefF

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,16 +5,16 @@ import axios from "axios"
 axios.defaults.withCredentials = true;
 
 export const axiosInstance = axios.create({
-//baseURL: "http://localhost:3000",
- baseURL: "https://kef.onrender.com",
+baseURL: "http://localhost:3000",
+  baseURL: "https://kef.onrender.com",
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   }
-})
-
+}) 
+ 
 export const axiosPrivateInstance = axios.create({
-  //baseURL: 'http://localhost:3000', // Make sure this matches your server's address
+  baseURL: 'http://localhost:3000', // Make sure this matches your server's address
    baseURL: "https://kef.onrender.com",
   withCredentials: true,
   headers: {
