c12904e16f1ae53a6235a348e8582f1cfd06fa5d
Artur Golovko <arturgolovko37@gmail.com>
2023-11-01 16:50:14 +0200

    Newrender

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -6,7 +6,7 @@ axios.defaults.withCredentials = true;
 
 export const axiosInstance = axios.create({
 //baseURL: "http://localhost:3000",
-  baseURL: "https://kef.onrender.com",
+  baseURL: "https://kefbackend.onrender.com",
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
@@ -15,7 +15,7 @@ export const axiosInstance = axios.create({
  
 export const axiosPrivateInstance = axios.create({
   //baseURL: 'http://localhost:3000', // Make sure this matches your server's address
-  baseURL: "https://kef.onrender.com",
+  baseURL: "https://kefbackend.onrender.com",
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
